a396b65eae751c34ee2264a8fb7b329f85d7917c
scormier1499 <sheila_cormier@hotmail.com>
2021-03-12 03:33:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainname = [];
+  let pronoun = ["the", "my"];
+  let adj = ["navy", "green"];
+  let noun = ["house", "computer"];
+  let extensions = [".com", ".us", ".net", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          domainname.push(pronoun[i] + adj[j] + noun[k] + extensions[l]);
+        }
+      }
+    }
+  }
+
+  console.log(domainname);
+
+  let html_element = document.querySelector("#display");
+
+  for (let i = 0; i < domainname.length; i++) {
+    html_element.innerHTML += "<li>" + domainname[i] + "</li>";
+  }
 };
